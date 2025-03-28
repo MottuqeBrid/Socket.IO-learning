34c231eeb8353847de73f345d85a07c67d807ca6
Mottuqe Brid <mottuqebrid@gmail.com>
2025-03-28 15:21:42 +0600

    broadcast

--- index.js
+++ index.js
@@ -10,50 +10,73 @@ const PORT = 3000;
 const { Server } = require("socket.io");
 const io = new Server(expressServer);
 
-// connect socket
-io.on("connection", (socket) => {
-  console.log(`new user connected`);
+// // connect socket
+// io.on("connection", (socket) => {
+//   console.log(`new user connected`);
 
-  //   setTimeout(() => {
-  //     socket.send(`Learn socket app (server to client) 1`);
-  //   }, 10000);
-  //   setInterval(() => {
-  //     socket.send(
-  //       `Learn socket app (server to client) number: ${Math.floor(
-  //         Math.random() * 100
-  //       )}`
-  //     );
-  //   }, 1000);
+//   setTimeout(() => {
+//     socket.send(`Learn socket app (server to client) 1`);
+//   }, 10000);
+//   setInterval(() => {
+//     socket.send(
+//       `Learn socket app (server to client) number: ${Math.floor(
+//         Math.random() * 100
+//       )}`
+//     );
+//   }, 1000);
 
-  //   setInterval(() => {
-  //     const d = new Date();
-  //     const t = d.getTime();
-  //     socket.send(`${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} ok ${t}`);
-  //   }, 100);
+//   setInterval(() => {
+//     const d = new Date();
+//     const t = d.getTime();
+//     socket.send(`${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} ok ${t}`);
+//   }, 100);
 
-  //   find socket clint List
-  //   setInterval(function () {
-  //     socket.send(`clint list is: ${[...io.sockets.sockets.keys()].length}`);
-  //   }, 100);
+//   find socket clint List
+//   setInterval(function () {
+//     socket.send(`clint list is: ${[...io.sockets.sockets.keys()].length}`);
+//   }, 100);
 
-  //   custom event
-  //   setInterval(() => {
-  //     socket.emit("myEvent", Math.floor(Math.random() * 1000));
-  //   }, 100);
+//   custom event
+//   setInterval(() => {
+//     socket.emit("myEvent", Math.floor(Math.random() * 1000));
+//   }, 100);
 
-  // receive message from client
-  //   socket.on("message", (mes) => {
-  //     console.log(mes);
-  //   });
+// receive message from client
+//   socket.on("message", (mes) => {
+//     console.log(mes);
+//   });
 
-  // receive message from server with custom event
-  socket.on("customEvent", (mes) => {
-    console.log(mes);
-  });
+// receive message from server with custom event
+// socket.on("customEvent", (mes) => {
+//   console.log(mes);
+// });
 
-  socket.on("disconnect", () => {
-    console.log(`user disconnected`);
-  });
+//   socket.on("disconnect", () => {
+//     console.log(`user disconnected`);
+//   });
+// });
+
+// socket broadcast
+// io.on("connection", (socket) => {
+//   console.log(`connection established`);
+// broadcast all Users
+// io.sockets.emit("myBroadcast", "hello everyone");
+
+//   socket.on("disconnect", () => {
+//     console.log(`user disconnected`);
+//   });
+// });
+
+// create namespace
+
+const buyNsp1 = io.of("/buy");
+const sellNsp1 = io.of("/sell");
+
+buyNsp1.on("connection", (socket) => {
+  buyNsp1.emit("myBroadcast", "hello everyone: buyNsp1");
+});
+sellNsp1.on("connection", (socket) => {
+  sellNsp1.emit("myBroadcast", "hello everyone: sellNsp1");
 });
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
